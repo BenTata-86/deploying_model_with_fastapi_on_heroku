0438013d75b9b7abc33c3c0a7ba3b06a4e1969dd
Taha Akgün <tahaakgun@Bengisu-MacBook-Air.local>
2022-06-06 17:25:53 +0300

    script added

--- src/train_model.py
+++ src/train_model.py
@@ -1,14 +1,17 @@
+import pandas as pd
+
+from joblib import dump
+
 from .ml.basic_cleaning import basic_cleaning
 from .ml.data import process_data
 from .ml.model import train_model
-from sklearn.model_selection import train_test_split
-# Add the necessary imports for the starter code.
+
+
 
 # Load data.
-data = basic_cleaning(path='../data/census.csv.dvc')
-# Train_test split.
-train, test = train_test_split(data, test_size=0.20)
+X = basic_cleaning("../data/census.csv.dvc")
 
+# Define categorical Features.
 cat_features = [
     "workclass",
     "education",
@@ -21,14 +24,13 @@ cat_features = [
 ]
 # Process the train data with the process_data function
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
-
-# Proces the test data with the process_data function.
-X_test, y_test,encoder, lb = process_data(
-    test, categorical_features=cat_features, label="salary", training=True
+    X, categorical_features=cat_features, label="salary", training=True
 )
 
 # Train and save a model.
 
+model = train_model(X_train, y_train)
 
+dump(model, f"{root_path}/model/model.joblib")
+dump(encoder, f"{root_path}/model/encoder.joblib")
+dump(lb, f"{root_path}/model/lb.joblib")
