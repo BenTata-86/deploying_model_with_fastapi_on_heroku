cb7391d227c6a85803539798f0a362e009d0b3f3
BenTata-86 <taha.akgun@hotmail.com>
2022-06-16 16:55:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ best_model = load('model/model.joblib')
 encoder = load('model/encoder.joblib')
 lb = LabelBinarizer()
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 app = FastAPI()
 
 
