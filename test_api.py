3c3d7348275e729b98e2ca0e7fe3746ff357c2fa
BenTata-86 <taha.akgun@hotmail.com>
2022-06-16 16:39:00 +0300

    updated test_api.py

--- test_api.py
+++ test_api.py
@@ -20,34 +20,36 @@ def test_get(client):
 
 
 def test_post_above(client):
-    request = client.post("/", json={'age': 40,
-                                     'workclass': 'Private',
-                                     'fnlgt': 149449,
-                                     'education': 'Bachelors',
-                                     'marital_status': 'Married-civ-spouse',
-                                     'occupation': 'Exec-managerial',
-                                     'relationship': 'Husband',
-                                     'race': 'White',
-                                     'sex': 'Male',
-                                     'hoursPerWeek': 45,
-                                     'nativeCountry': 'United-States'
-                                     })
+    data = {
+        'age': 40,
+        'workclass': 'Private',
+        'fnlgt': 149596,
+        'education': 'Bachelors',
+        'marital-status': 'Married-civ-spouse',
+        'occupation': 'Exec-managerial',
+        'relationship': 'Husband',
+        'race': 'White',
+        'sex': 'Male',
+        'hours-per-week': 45,
+        'native-country': 'United-States'
+        }
+    request = client.post("/income_class", json=data)
     assert request.status_code == 200
     assert request.json() == {"prediction": "above 50K$"}
 
 
 def test_post_below(client):
-    request = client.post("/", json={'age': 18,
-                                     'workclass': 'Private',
-                                     'fnlgt': 14184,
-                                     'education': 'HS-grad',
-                                     'marital_status': 'Never-married',
-                                     'occupation': 'Sales',
-                                     'relationship': 'Not-in-family',
-                                     'race': 'Black',
-                                     'sex': 'Male',
-                                     'hoursPerWeek': 40,
-                                     'nativeCountry': 'Cuba'
-                                     })
+    request = client.post("/income_class", json={'age': 18,
+                                                 'workclass': 'Private',
+                                                 'fnlgt': 14184,
+                                                 'education': 'HS-grad',
+                                                 'marital-status': 'Never-married',
+                                                 'occupation': 'Sales',
+                                                 'relationship': 'Not-in-family',
+                                                 'race': 'Black',
+                                                 'sex': 'Male',
+                                                 'hours-per-week': 40,
+                                                 'native-country': 'Cuba'
+                                                 })
     assert request.status_code == 200
-    assert request.json() == {"prediction": "equal or below  50K$'"}
+    assert request.json() == {"prediction": "equal or below  50K$"}
