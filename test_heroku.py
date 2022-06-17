dd3ac3a32a4f74cfa9fd0cf0c66ab8b6c5684932
BenTata-86 <taha.akgun@hotmail.com>
2022-06-17 11:22:14 +0300

    Update test_heroku.py

--- test_heroku.py
+++ test_heroku.py
@@ -1,5 +1,4 @@
-#import requests
-'''
+import requests
 data = {
             'age': 35,
             'workclass': 'Private',
@@ -20,4 +19,3 @@ assert r.status_code == 200
 
 print("Response code: %s" % r.status_code)
 print("Response body: %s" % r.json())
-'''
