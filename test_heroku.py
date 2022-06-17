6e5904ff21a09effd7839790435fe19f8af0c1f6
BenTata-86 <taha.akgun@hotmail.com>
2022-06-17 09:22:10 +0300

    Update Aptfile

---
+++ test_heroku.py
@@ -0,0 +1,22 @@
+import requests
+
+data = {
+            'age': 35,
+            'workclass': 'Private',
+            'fnlgt': 149184,
+            'education': 'HS-grad',
+            'marital_status': 'Never-married',
+            'occupation': 'Sales',
+            'relationship': 'Not-in-family',
+            'race': 'White',
+            'sex': 'Male',
+            'hoursPerWeek': 60,
+            'nativeCountry': 'United-States'
+    }
+
+r = requests.post('https://census-income-fastapi-app.herokuapp.com/', json=data)
+
+assert r.status_code == 200
+
+print("Response code: %s" % r.status_code)
+print("Response body: %s" % r.json())
