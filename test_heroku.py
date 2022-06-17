b93d8960af00d43a226bea5ffbfcedb5e68a6383
BenTata-86 <taha.akgun@hotmail.com>
2022-06-17 12:02:56 +0300

    update test_heroku.py

--- test_heroku.py
+++ test_heroku.py
@@ -5,19 +5,20 @@ data = {
             'age': 35,
             'workclass': 'Private',
             'fnlgt': 149184,
-            'education': 'HS-grad',
-            'marital_status': 'Never-married',
+            'education': 'Bachelors',
+            'marital-status': 'Never-married',
             'occupation': 'Sales',
             'relationship': 'Not-in-family',
             'race': 'White',
             'sex': 'Male',
-            'hoursPerWeek': 60,
-            'nativeCountry': 'United-States'
-    }
+            'hours-per-week': 60,
+            'native-country': 'United-States'
+        }
 
-r = requests.post('https://census-income-fastapi-app.herokuapp.com/income_class/', data=json.dumps(data))
+r = requests.post('https://census-income-fastapi-app.herokuapp.com/income_class/', json=data)
 
-assert r.status_code == 200
 
 print("Response code: %s" % r.status_code)
 print("Response body: %s" % r.json())
+
+assert r.status_code == 200
